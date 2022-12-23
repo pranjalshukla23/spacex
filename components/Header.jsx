708723b9aed20492ff4f0d50d3f9d9f08e5abcb2
pranjalshukla23 <62809277+pranjalshukla23@users.noreply.github.com>
2022-12-23 07:52:39 +0530

    fixed nav links

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,22 +1,44 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 export const Header = () => {
   const [id, setId] = useState("");
+
+  const router = useRouter();
+
   return (
     <header className="flex flex-col md:flex-row bg-black p-2 text-white flex items-center text-center mb-24 z-50">
       <Image src="/images/logo.jpg" alt="logo" width={100} height={100}></Image>
 
       <div className="flex flex-col md:flex-row justify-around justify-between items-center text-center w-full font-bebas text-2xl">
         <Link href="/">
-          <div>Home</div>
+          <div
+            className={`hover:text-red-400 ${
+              router.pathname === "/" ? "text-red-400" : ""
+            }`}
+          >
+            Home
+          </div>
         </Link>
         <Link href="/history">
-          <div>History</div>
+          <div
+            className={`hover:text-red-400 ${
+              router.pathname === "/history" ? "text-red-400" : ""
+            }`}
+          >
+            History
+          </div>
         </Link>
         <Link href="/payload">
-          <div>Payload</div>
+          <div
+            className={`hover:text-red-400 ${
+              router.pathname === "/payload" ? "text-red-400" : ""
+            }`}
+          >
+            Payload
+          </div>
         </Link>
         <div className="p-2 flex justify-center items-center gap-2">
           <input
@@ -26,7 +48,6 @@ export const Header = () => {
             placeholder="search history by id"
             value={id}
             onChange={(e) => setId(e.target.value)}
-            required
           />
           <Link href={`/history/${id}`}>
             <i className="fa-solid fa-magnifying-glass"></i>
