5be92363c467a2118a31673c8be41bf6a864a3d6
pranjal-betaflux <pranjal.shukla@betaflux.co>
2023-11-04 22:32:44 +0530

    Added google analytics integration

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,10 @@ import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { Provider } from "react-redux";
 import store from "../app/store";
+import ReactGA from "react-ga4";
 
 function MyApp({ Component, pageProps }) {
+  ReactGA.initialize("G-FZ3RP5JTN1");
   return (
     <Provider store={store}>
       <Layout>
